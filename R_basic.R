e68122b31f3a08dc4297f6409b1c0c76933f9dd9
Zomath <duansongtoki@icloud.com>
2018-07-27 17:38:38 +0800

    diaun

--- R_basic.R
+++ R_basic.R
@@ -56,3 +56,93 @@ quarters(x3)
 julian(x3)
 x4 <- as.Date("1999-05-24")
 x2-x4
+
+#Subsetting List
+x <- list(id= 1:4,height = 170,gender = "male")
+x[1]
+x[[1]]
+x$height
+x[c(1,2)]
+y <- "id"
+x[["id"]]
+x[[y]]
+
+#Missing value
+x <- c(1,NA,2,NA)
+is.na(x)
+x[!is.na(x)]
+y <- c(NA,2,3,NA)
+z <- complete.cases(x,y)
+x[z]
+y[z]
+
+#example
+library(datasets)
+head(airquality)
+g <- complete.cases(airquality)
+airquality[g,][1:10,]
+airquality[g,]
+
+#Vectorized
+x <- 1:5
+y <- 6:10
+x + y
+x/y
+x*y
+x <- matrix(1:4,2,2)
+y <- matrix(rep(2,4),2,2)
+x*y
+x/y
+x %*% y
+
+#lapply
+str(lapply)
+x <- list(a = 1:10,b=c(11,21,31,41,51))
+lapply(x, mean)
+x <- 1:4
+lapply(x,runif,0,100)
+
+x <- list(a = matrix(1:6,2,3),b = matrix(4:7,2,2))
+lapply(x,function(a) a[1,])
+
+#sapply
+x <- list(a = 1:10,b=c(11,21,31,41,51))
+z <- sapply(x, mean)
+
+#apply
+x <- matrix(1:16,4,4)
+apply(x,2,mean)
+rowSums(x)
+colMeans(x)
+x <- matrix(rnorm(100),10,10)
+apply(x,1,quantile,probs=c(0.25,0.75))
+x <- array(rnorm(2*3*4),c(2,3,4))
+apply(x,c(1,2),mean)
+apply(x,c(1,3),mean)
+
+#mapply
+x <- list(rep(1,4),rep(2,3),rep(3,2),rep(4,1))
+mapply(rep,1:4,4:1)
+
+s <- function(n,mean,std){
+  rnorm(n,mean,std)
+}
+hist(s(100,3,3))
+mapply(s, 1:5, 5:1, 2)
+
+#tapply
+str(tapply)
+x <- c(rnorm(5), runif(5), rnorm(5,1))    
+f <- gl(3,5)
+
+tapply(x,f,mean,simplify = T)
+
+#split
+x <- c(rnorm(5), runif(5), rnorm(5,1))
+f <- gl(3,5)
+split(x,f)
+sapply(split(x,f), mean)
+head(airquality)
+s <- split(airquality,airquality$Month)
+table(airquality$Month)
+lapply(s, function(x) colMeans(x[,c("Ozone", "Wind", "Temp")]))
