0d4f28c0f2c6c84640c931851438da2c9b58767d
Zomath <duansongtoki@icloud.com>
2018-07-29 21:13:41 +0800

    daun

--- First_week.R
+++ First_week.R
@@ -20,3 +20,27 @@ summary(ft$X400.173700)
 summary(ft$X73.476760)
 plot(ft$X400.173700,ft$X73.476760, lwd = 1, col = "steelblue")
 ggplot(ft,aes(X400.173700,X73.476760)) + stat_smooth()
+boxplot(ft$X73.476760)
+row.names(ft)
+y <- data.matrix(ft)
+class(y)
+y
+x <- data.frame(foo = 1:4, bar = c("T", "T", "F", "F"))
+x
+nrow(x)
+ncol(x)
+x <- 1:3
+y <- 4:6
+cbind(x,y)
+m <- 1:6
+m
+dim(m) <- c(2,3)
+m
+x <- 1:3
+names(x)
+names(x) <- c("foo", "bar", "norf")
+x
+names(x)
+ft
+names(ft) <- c("x", "y")
+head(ft)
